f6e05c145cd812b68e48c9b04063fe41b1989e1f
MartinCalamel <martin.calamel@cpe.fr>
2025-11-06 15:54:19 +0100

    Enhance fichier.c with comments for clarity
    Added comments to clarify file operations and buffer usage.

--- TP4/src/fichier.c
+++ TP4/src/fichier.c
@@ -1,3 +1,7 @@
+/* fichier.c
+* Auteur : Armand
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "fichier.h"
@@ -7,8 +11,11 @@ void lire_fichier (char * nom){
 	fseek(f, 0, SEEK_END);
 	long taille = ftell(f);
 	rewind(f);
-
+	
+	// utilisation de la taille du fichier pour definir le buffer 
 	char *buffer = malloc(taille + 1);
+
+	// lecture du fichier avec fread
 	size_t lus = fread(buffer, 1, taille, f);
 	buffer[lus] = '\0';
 
@@ -19,6 +26,7 @@ void lire_fichier (char * nom){
 }
 
 void ecrire_dans_fichier(char*nom, char*texte ){
+	 // érciture dans le fichier avec fprintf
 	 FILE *fichier = fopen(nom, "w");
 	 fprintf(fichier, "%s", texte);
 	 fclose(fichier);
